c689f513f263a80a0912196f0a88f97cccdd15c6
Zuhaib Sheikh Mahmood <mahmoo21@msu.edu>
2018-08-19 23:00:50 -0400

    updated eucdist and cossim

--- zmdatafunctions.R
+++ zmdatafunctions.R
@@ -18,10 +18,52 @@ rescale<-function(olddata,newmean,newsd){
 ###########
 #Calculate euclidean distance
 ###########
+cossim<-function(x,y){
+	#########################
+	#Calculate cosine simimilarity in N-dimensional space from X to Y,
+	#	where x and y are dataframes or matrices
+	#########################
+	if( typeof(x)=="double" & length(x)>1 & is.null(dim(x)) ){
+		warning("Treating first input as single vector")
+		.matX<-t(matrix(x))
+	}else{
+		.matX<-as.matrix(x)
+	}
+	
+	if( typeof(y)=="double" & length(y)>1 & is.null(dim(y)) ){
+		warning("Treating second input as single vector")
+		.matY<-t(matrix(y))
+	}else{
+		.matY<-as.matrix(y)
+	}
+	
+	.num<-rowSums(.matX*.matY)
+	.den<-sqrt(rowSums(.matX^2))*sqrt(rowSums(.matY^2))
+	.out<-.num/.den
+	names(.out)<-NULL
+	return(.out)
+}
+
 eucdist<-function(x,y){
-	#Calculate euclidean distance in N-dimensional space from x to y, where x and y are dataframes or matrices.
-	.matX<-as.matrix(x)
-	.matY<-as.matrix(y)
+	################
+	#Calculate euclidean distance in N-dimensional space from x to y, 
+	#	where x and y are dataframes or matrices.
+	################
+	if( typeof(x)=="double" & length(x)>1 & is.null(dim(x)) ){
+		warning("Treating first input as single vector")
+		.matX<-t(matrix(x))
+	}else{
+		.matX<-as.matrix(x)
+	}
+	
+	if( typeof(y)=="double" & length(y)>1 & is.null(dim(y)) ){
+		warning("Treating second input as single vector")
+		.matY<-t(matrix(y))
+	}else{
+		.matY<-as.matrix(y)
+	}
+
+	stopifnot(dim(.matX)==dim(.matY))
 	.matDiff<-(.matX-.matY)^2
 	.eucdistSQ<-rowSums(.matDiff)
 	.eucdist<-sqrt(.eucdistSQ)
